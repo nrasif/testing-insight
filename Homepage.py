64725cc64574cbe5383f04a892e8ef1e786272a4
naharirasif <naharirasif18@gmail.com>
2025-01-24 19:16:26 +0700

    Initial Commit

---
+++ Homepage.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from components.logo_sidebar import get_logo
+
+st.set_page_config(page_title='BSI Testing Insight', layout='wide')
+
+get_logo('assets/testinginsight.png')
+
+
+
+
+
+
---
+++ pages/1_UAT.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from components.logo_sidebar import get_logo
+
+st.set_page_config(page_title='BSI Testing Insight', layout='wide')
+
+get_logo('assets/testinginsight.png')
+
+st.write('Anggoran tai')
