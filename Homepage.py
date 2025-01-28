f549e18987dbdb3477acd21d8a445e197dc8abc9
naharirasif <naharirasif18@gmail.com>
2025-01-28 15:58:47 +0800

    add: title card

--- Homepage.py
+++ Homepage.py
@@ -9,7 +9,21 @@ get_logo('assets/testinginsight.png')
 with open('css/style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html = True)
 
+date = '28/01/2025'
 
+# Text title
+st.markdown(
+    f"""
+    <p class='uat_line'>
+        <span class='uat_text'>UAT</span>
+        <span class='card_title'>Progress {date}</span>
+    </p>
+    """,
+    unsafe_allow_html=True
+)
+
+
+# Card Component
 cards = [
     {"text": "Execution", "value": "88%", "delta": "+10%", "help_text": "Test case execution progress"},
     {"text": "Passed", "value": "53%", "delta": "+11%", "help_text": "Percentage of test cases successfully executed without issues"},
